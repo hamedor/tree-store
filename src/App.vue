1ed5b93dbc2517ca5bf5cb6c7012420dbecb0cf9
dmitri <popov.d@flgchina.ru>
2024-12-25 21:07:14 +0500

    feat: project preparing

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <script setup lang="ts">
+
+
+import TreeTable from "./components/TreeTable.vue";
 </script>
 
 <template>
-
+  <div class="app">
+    <TreeTable />
+  </div>
 </template>
 
 <style scoped>
+.app {
+  width: 100%;
+  height: 1000px;
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { ModuleRegistry, ClientSideRowModelModule, TreeDataModule } from 'ag-grid-enterprise'
 
+
+ModuleRegistry.registerModules([ClientSideRowModelModule, TreeDataModule])
 createApp(App).mount('#app')
